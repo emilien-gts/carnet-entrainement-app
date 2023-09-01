fbe735bb663fe491ca99934ef00f822ae98ac761
EmilienGts <emilien.gantois.pro@gmail.com>
2023-09-01 22:14:28 +0200

    Init project

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/dashboard')]
+    public function index(): Response
+    {
+        return $this->render('dashboard/index.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        $dashboard = Dashboard::new();
+        $dashboard->setTitle('Carnet d\'entrainement');
+
+        return $dashboard;
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
